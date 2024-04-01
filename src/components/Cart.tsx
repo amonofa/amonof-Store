c19071436ac4dbf0e2938ae43ef5cdc22f8bdd51
amonof_a <amonovabu74@gmail.com>
2024-04-01 18:35:26 +0500

    upadate

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -58,7 +58,7 @@ const Cart = () => {
   );
   const handleCheckout = async () => {
     const stripe = await stripePromise;
-    const response = await fetch("http://localhost:3000/api/checkout", {
+    const response = await fetch("https://amonof-store.vercel.app//api/checkout", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
